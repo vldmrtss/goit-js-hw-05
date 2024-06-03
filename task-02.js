fae868152df78430b71c148f3da0deb1a8ded035
vldmrtss <vldmrtss@gmail.com>
2024-06-03 18:33:50 +0200

    done?

---
+++ task-02.js
@@ -0,0 +1,39 @@
+const getUsersWithFriend = (users, friendName) => {
+  const filterFriends = users.filter((user) =>
+    user.friends.includes(friendName)
+  );
+  return filterFriends;
+};
+
+const users = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"],
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"],
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"],
+  },
+];
+
+console.log(getUsersWithFriend(users, "Briana Decker"));
